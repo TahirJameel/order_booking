ed06b2fa7657b83d47cb99ceee9fe159bf58bdde
Tahir Jameel <126069935+TahirJameel@users.noreply.github.com>
2024-03-07 12:04:36 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'login.dart';
+
+import 'splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,34 +18,13 @@ class MyApp extends StatelessWidget {
       title: 'X Order',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         textTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme),
         scaffoldBackgroundColor: const Color(0xffeef0f2),
         appBarTheme: const AppBarTheme(color: Color(0xffeef0f2)),
         useMaterial3: true,
       ),
       home: const SplashScreen(),
-    );
-  }
-}
-
-// Splash Screen Activity
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    Timer(const Duration(seconds: 2), () {
-      Get.offAll(const Login());
-    });
-    return Scaffold(
-      body: Center(
-        child: Image.asset(
-          'assets/images/softtouch_black.png',
-          scale: 1.3,
-        ),
-      ),
     );
   }
 }
